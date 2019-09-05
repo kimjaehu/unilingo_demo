80813dff5605f6f7414b26baf06ca27fe63301b9
Jay Kim <kimjaehu@gmail.com>
2019-09-05 03:40:49 -0400

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Login from './components/Login';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Login />
-      </div>
+      <BrowserRouter>
+        <div className='App'>
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
