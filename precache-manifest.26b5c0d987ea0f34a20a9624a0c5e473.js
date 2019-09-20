91cbb686595a63255461b0f73803f18ffdac3654
Jay Kim <kimjaehu@gmail.com>
2019-09-19 23:38:35 -0400

    Updates

---
+++ precache-manifest.26b5c0d987ea0f34a20a9624a0c5e473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1211a19178d1483a48e0312d5223fc",
+    "url": "/unilingo_demo/index.html"
+  },
+  {
+    "revision": "32cf2a68671c83f9d9b2",
+    "url": "/unilingo_demo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7222bbc3ad40fd97f959",
+    "url": "/unilingo_demo/static/js/2.fc5027fd.chunk.js"
+  },
+  {
+    "revision": "32cf2a68671c83f9d9b2",
+    "url": "/unilingo_demo/static/js/main.62a813e0.chunk.js"
+  },
+  {
+    "revision": "eb09ba89ac759cbcdaf4",
+    "url": "/unilingo_demo/static/js/runtime~main.2e3eeaa8.js"
+  }
+]);
